fa067871df14ec4eb63c6676d51ff12d0455d5ce
amitanshusahu <amitanshusahu.official@gmail.com>
2024-07-22 21:57:40 +0530

    fix

--- components/micro-Ui/ImageGrid.jsx
+++ components/micro-Ui/ImageGrid.jsx
@@ -5,8 +5,8 @@ export default function ImageGrid({ images }) {
     <div className="container mx-auto p-6">
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {images.map((image, index) => (
-          <div className="grad p-[2px] rounded-md pop">
-            <div key={index} className="rounded-md w-full h-80 flex items-center justify-center overflow-hidden">
+          <div key={index} className="grad p-[2px] rounded-md pop">
+            <div className="rounded-md w-full h-80 flex items-center justify-center overflow-hidden">
             <Image
               src={image}
               alt={`Image ${index}`}
